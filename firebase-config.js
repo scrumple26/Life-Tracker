5e489aac88798734cd317f84c3db54f275ed648e
Nolan Leyse <nolanl@adamspestcontrol.com>
2026-04-24 09:59:30 -0500

    Switch to dedicated life-tracker-6f707 Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.LIFE_TRACKER_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCp1WYms8u5dkUg-3zLcRWX5v11iglF1-U",
-  authDomain: "goal-tracker-d0a58.firebaseapp.com",
-  projectId: "goal-tracker-d0a58",
-  storageBucket: "goal-tracker-d0a58.firebasestorage.app",
-  messagingSenderId: "891314847608",
-  appId: "1:891314847608:web:61c63ac9e6db74554c528d"
+  apiKey: "AIzaSyD4f98h80C4QZbBKMef4Upz5-R6T6TyJIg",
+  authDomain: "life-tracker-6f707.firebaseapp.com",
+  projectId: "life-tracker-6f707",
+  storageBucket: "life-tracker-6f707.firebasestorage.app",
+  messagingSenderId: "508282035523",
+  appId: "1:508282035523:web:cd2981dcd973ddf93fe3f6"
 };
